ecb68b9a0e62d055ba41e1aeefc6365066f511d4
Ratheven Sivarajah <94582313+Ratheven@users.noreply.github.com>
2022-08-22 16:53:46 -0400

    display-information

---
+++ client/src/SearchBar.js
@@ -0,0 +1,15 @@
+import "./SearchBar.css"
+
+const SearchBar = () => {
+  return (
+    <>
+      <div className="container">
+        <form className="search=form">
+          <input className="search-bar" />
+          <button className="search-button"></button>
+        </form>
+      </div>
+    </>
+  );
+};
+export default SearchBar;
