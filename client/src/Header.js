8b244c31cc9752a4130612df625e704b0928effa
Ratheven Sivarajah <94582313+Ratheven@users.noreply.github.com>
2022-08-30 17:12:52 -0400

    recieved-info-from-backend

--- client/src/Header.js
+++ client/src/Header.js
@@ -1,16 +1,36 @@
-import "./Header.css"
+import "./css/Header.css";
 import LoginButton from "./LoginButton";
 import LogoutButton from "./LogoutButton";
 import SearchBar from "./SearchBar";
+import { useHistory } from "react-router-dom";
 
-
-const Header = ({ search, setSearch, setQuery })=> {
-    return(
-        <div className="container">
-            <SearchBar search={search} setSearch={setSearch} setQuery={setQuery}/>
-            <LoginButton/>
-            <LogoutButton/>
+const Header = ({ search, setSearch, setQuery }) => {
+  let history = useHistory();
+  return (
+    <>
+      <div className="umber-box">Hello</div>
+      <div className="container-header">
+        <div onClick={()=>history.push("/")}>Logo</div>
+        <div className="search-headers">
+          <div className="searchbar-margin">
+            <SearchBar
+            
+              search={search}
+              setSearch={setSearch}
+              setQuery={setQuery}
+            />
+          </div>
+          <LoginButton />
+          <LogoutButton />
         </div>
-    )
-}
-export default Header
+      </div>
+      <div className="list">
+
+      <p  onClick={() => history.push("/groceryList")}>Grocery List</p>
+      <p className="notes-list">notes</p>
+      <p>schedule</p>
+      </div>
+    </>
+  );
+};
+export default Header;
--- client/src/LoginButton.js
+++ client/src/LoginButton.js
@@ -1,10 +1,11 @@
+import styled from "styled-components";
 import { useAuth0 } from "@auth0/auth0-react";
 const LoginButton = ()=> {
     const { isAuthenticated, loginWithRedirect } = useAuth0();
 
     return (
         !isAuthenticated && (
-          <button onClick={() => loginWithRedirect()}>Log In</button>
+          <p className="header-sign-in" onClick={() => loginWithRedirect()}>Sing In</p>
         )
       );
 }
